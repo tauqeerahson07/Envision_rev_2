156706bedf5f1f433cbc1964fd0e9c3b5ff7554a
Tauqeer Ahson <97336093+tauqeerahson07@users.noreply.github.com>
2026-03-23 20:06:14 +0500

    Remove pip, setuptools, and wheel upgrade from build script

--- backend/build.sh
+++ backend/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Ensure pip, setuptools and wheel are up-to-date so pkg_resources is available
-python -m pip install --upgrade pip setuptools wheel
-
 # Install dependencies
 pip install -r requirements.txt
 
